e3d4903cfe575164294059d0ebbac9f4a14f06e0
SebTech33 <sebtech33@gmail.com>
2024-01-16 21:21:04 +0100

    Updated path

--- Documents/PowerShell/MyModules/starship.psm1
+++ Documents/PowerShell/MyModules/starship.psm1
@@ -1,4 +1,4 @@
 # Only needed if you want to change the default path of starship
 # Default path: $HOME\config\starship.toml
-$ENV:STARSHIP_CONFIG = "$HOME\.dotfiles\starship.toml"
+$ENV:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
 Invoke-Expression (&starship init powershell)
