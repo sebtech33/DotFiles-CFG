f72904db90b2cf38cd9596183e613903ba95d0d1
Sebastian Hansen Eliassen <sebastiel@viken.no>
2024-01-18 15:30:37 +0100

    Added more to GitHubRelease script, but as of not not worky!

--- Documents/PowerShell/MyScripts/Get-GitHubRelease.ps1
+++ Documents/PowerShell/MyScripts/Get-GitHubRelease.ps1
@@ -1,4 +1,25 @@
+function Show-GitHubReleases() {
+    [CmdletBinding()]
+    param(
+        [Parameter(Position = 0, mandatory = $true)]
+        [string] $repo
+    )
+    $json = curl -s https://api.github.com/repos/$repo/releases/latest
+}
+
 function Get-GitHubRelease() {
-    param ($repo, $fileName)
+    [CmdletBinding()]
+    param(
+        [Parameter(Position = 0, mandatory = $true)]
+        [string] $repo,
+        [Parameter(Position = 1)]
+        [string] $fileName
+    )
+    if (!$fileName){
+        Show-GitHubReleases
+    }
+    
+    
     curl -LO https://github.com/$repo/releases/latest/download/$fileName
+
 }
