49748d199060f3cfe656d2b84861455bcb76546d
Sebastian Eliassen <sebtech33@gmail.com>
2024-04-10 22:52:12 +0200

    Fixed I think

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -17,3 +17,7 @@ Import-Module "$ModuleFolder\DotFiles-CFG.psm1"
 
 # Get-GitHubRelease
 Import-Module "$ScriptFolder\Get-GitHubRelease.ps1"
+#34de4b3d-13a8-4540-b76d-b9e8d3851756 PowerToys CommandNotFound module
+
+Import-Module "C:\Users\SebTech33\AppData\Local\PowerToys\WinUI3Apps\..\WinGetCommandNotFound.psd1"
+#34de4b3d-13a8-4540-b76d-b9e8d3851756
